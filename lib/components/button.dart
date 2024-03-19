49fc4bebad6a58bef2494759699ffe50538fedb6
sinanengin <sinan.engin.02@gmail.com>
2024-03-19 21:31:00 +0300

    Intro Page tamamlandı.

---
+++ lib/components/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  const MyButton({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 201, 201, 201),
+          borderRadius: BorderRadius.circular(40)),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            text,
+            style: TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.arrow_forward,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,29 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kaffe/components/button.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: IntroPage(),debugShowCheckedModeBanner: false,), // use MaterialApp
+    const MaterialApp(
+      home: IntroPage(),
+      debugShowCheckedModeBanner: false,
+    ), // use MaterialApp
   );
 }
 
-class IntroPage extends StatelessWidget{
+class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      body: Padding(
-        padding: EdgeInsets.all(25.0),
-      child: Column(
-        children: [
-          SizedBox(height: 25),
-        // marka ismi
-        Text("kaffe")
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 240, 240, 240),
+        body: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 25),
+              // marka ismi
 
-      ],),
-      )
-    );
-    
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: Image(
+                      image: AssetImage('lib/images/coffee-cup.png'),
+                    ),
+                  ),
+                  const SizedBox(width: 5), // İstenirse bir boşluk eklenebilir
+                  Text(
+                    "kaffe",
+                    style: GoogleFonts.inconsolata(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Image.asset('lib/images/coffee-beans.png'),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              Text(
+                "Kaffe keyfini evinde yaşa!",
+                style: GoogleFonts.inconsolata(
+                  fontSize: 44,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              Text(
+                "Eşsiz lezzetin keyfine varmak için Kaffe lezzeti şimdi bir tıkla kapınızda!",
+                style: GoogleFonts.inconsolata(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              MyButton(text: "Hadi Başlayalım!")
+            ],
+          ),
+        ));
   }
 }
